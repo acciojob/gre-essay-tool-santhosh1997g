ddd325a70992da5b66bfc9956c33f1b55ad8803a
Guntur Santhosh Kumar <98687152+santhosh1997g@users.noreply.github.com>
2023-01-21 21:27:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your code here
+//your code here
+const input_area = document.getElementById("evaluatedText");
+const wordCount=document.getElementById("wordCount");
+
+
+
+input_area.addEventListener("input",()=>{ 
+    let txt =input_area.value.trim();
+    wordCount.textContent=txt.split(/\s+/).filter(
+        (item) => item).length;
+    });
